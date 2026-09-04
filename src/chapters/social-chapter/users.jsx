165d998431c98042fe8e93d51b49cb38f69c850c
Shafeeq-lynxbro <Shalbia549@gmail.com>
2026-09-04 17:51:49 +0700

    Update users.jsx

--- src/chapters/social-chapter/users.jsx
+++ src/chapters/social-chapter/users.jsx
@@ -7,12 +7,3 @@ export default function Users({ selectedUser }) {
         <p>{selectedUser?.email || 'No email available.'}</p>
     </div>
     )}
-
-function Users({ selectedUser }) {
-  return(
-    <div className="users-container">
-        <h1>Youre on {selectedUser?.displayName || 'User'}'s profile</h1>
-        <h3>{selectedUser?.email || 'No email available.'}</h3>
-        <p>{selectedUser?.bio || 'No bio available.'}</p>
-    </div>
-    )}
